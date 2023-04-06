fea476d1ab8e0d42a5fc5fbe682b1ec95433c8c0
MrBled <bledc@tcd.ie>
2023-04-06 17:58:58 +0100

    added img folder

--- test_denoiser.py
+++ test_denoiser.py
@@ -10,7 +10,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
 
 if __name__ == '__main__':
-    dataset_path = "/home/user/placeholder"
+    dataset_path = "/home/bledc/dataset/wiener_demo_gaussian_imgs"
     test_dataset = load_noise_imgs(dataset_path, 100)
 
     test_loader = torch.utils.data.DataLoader(
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     print(device)
 
-    std = 50
+    std = 35
     win_size=32
     overlap=4
     wiener_denoiser = nn.DataParallel(Wiener_3D(0.3, 0.6, win_size, overlap)).cuda()
